af63c678f8472c54b8ed4416aefc64862fb71940
grantgolden13 <grantgolden13@gmail.com>
2022-06-25 12:44:46 -0700

    change variable names for better readability

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const form = document.querySelector('form');
 
 const email = document.getElementById('email');
-const emailError = document.querySelector('#email + span.error');
+const emailErrorSpan = document.querySelector('#email + span.error');
 
 const zip = document.getElementById('zip');
-const zipError = document.querySelector('#zip + span.error');
+const zipErrorSpan = document.querySelector('#zip + span.error');
 
 email.addEventListener('input', function(e) {
     if (email.validity.valid) {
-        emailError.textContent = "";
-        emailError.className = "error";
+        emailErrorSpan.textContent = "";
+        emailErrorSpan.className = "error";
     } else {
         showEmailError();
     }
@@ -17,8 +17,8 @@ email.addEventListener('input', function(e) {
 
 zip.addEventListener('input', function(e) {
     if(zip.validity.valid) {
-        zipError.textContent = "";
-        zipError.className = "error";
+        zipErrorSpan.textContent = "";
+        zipErrorSpan.className = "error";
     } else {
         showZipError();
     }
@@ -36,21 +36,21 @@ form.addEventListener('submit', function(e) {
 
 const showEmailError = function(e) {
     if (email.validity.valueMissing) {
-        emailError.textContent = "Please enter an email address.";
+        emailErrorSpan.textContent = "Please enter an email address.";
     } else if (email.validity.typeMismatch) {
-        emailError.textContent = "Value needs to be an email address";
+        emailErrorSpan.textContent = "Value needs to be an email address";
     } else if (email.validity.tooShort) {
-        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+        emailErrorSpan.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
     }
-    emailError.className = "error active";
+    emailErrorSpan.className = "error active";
 }
 
 const showZipError = function(e) {
     if (zip.validity.valueMissing) {
-        zipError.textContent = "Please enter your zip code";
+        zipErrorSpan.textContent = "Please enter your zip code";
     } else if (zip.validity.typeMismatch) {
-        zipError.textContent = "Value needs to be a zip code";
+        zipErrorSpan.textContent = "Value needs to be a zip code";
     } else if(zip.validity.tooShort) {
-        zipError.textContent = `Zip Code should be at least ${zip.minLength} numbers; you entered ${zip.value.length}.`
+        zipErrorSpan.textContent = `Zip Code should be at least ${zip.minLength} numbers; you entered ${zip.value.length}.`
     }
  }
